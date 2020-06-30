2f24a49bcd6facffb900424d589a0f2646bead97
Ying <sunyingnupt@163.com>
2020-06-30 15:19:45 +0800

    Update demo.m

--- demo.m
+++ demo.m
@@ -18,18 +18,18 @@ result={'target', 'F_measure','g_measure','AUC'};
                     src=project(s1).Dataset;
       
                 sname=project(s1).Name;
-                src=src(randperm(size(src,1)),:);
+     
                 tgt=project(t1).Dataset;
-                tname=project(t1).Name;
                 tgt=tgt(randperm(size(tgt,1)),:);
+                tname=project(t1).Name;
+               
                 
                 fs=src(:,1:size(src,2)-1);
                 fs = fs ./ repmat(sum(fs,2),1,size(fs,2));
                 Xs = zscore(fs,1);
                 Ys = src(:,size(src,2));
                 Ys=Ys+1;
-                legth=size(fs,1);leg_train=ceil(ratio*legth);
-                Xs=Xs(1:leg_train,:);Ys=Ys(1:leg_train,:);
+               
                 
                 
                 ft=tgt(:,1:size(tgt,2)-1);
@@ -37,7 +37,10 @@ result={'target', 'F_measure','g_measure','AUC'};
                 Xt = zscore(ft,1);
                 Yt = tgt(:,size(tgt,2));
                 Yt=Yt+1;
-              
+                
+                legth=size(ft,1);leg_test=ceil(ratio*legth);
+                Xt=Xs(1:leg_train,:);Yt=Yt(1:leg_train,:);
+                
                 options.d=5;%manifold
                 options.dim =3;%A
                 options.rho = 0.1;
